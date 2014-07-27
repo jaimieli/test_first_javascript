6be082cd118472f0e527c00f59b82dea1e5b8ff1
Jaimie Li <jaimieli@gmail.com>
2014-07-27 19:23:09 -0400

    Cleaner code

--- 08_mammals/mammal.js
+++ 08_mammals/mammal.js
@@ -4,22 +4,24 @@ function Mammal (name) {
 	this.sayHello = function () {
 		return "My name is " + this.name + ", I'm a Mammal";
 	}
+	this.haveBaby = function () { 
+		// extends the Mammal prototype with a method, haveBaby, added to the Mammal prototype
+		var newBaby = new Mammal("Baby "+this.name);
+		// newBaby is an instance of Mammal with the name "Baby" + parent's name
+		this.offspring.push(newBaby);
+		// pushes newBaby's name to the offspring array
+		return newBaby;
+	} 
 }
-
-Mammal.prototype.haveBaby = function () { 
-	var newBaby = new Mammal("Baby "+this.name);
-	this.offspring.push(newBaby);
-	return newBaby;
-} 
 
 function Cat (name, color) {
 	this.name = name;
 	this.color = color;
-	this.offspring = []
+	// this.offspring = [] // not needed since it'll inherit the method from the Mammal constructor
 }
 
 Cat.prototype = new Mammal(); // inherits all the methods from Mammal
-Cat.prototype.constructor = Cat; // ensures that instanceof works properly
+// Cat.prototype.constructor = Cat; // ensures that instanceof works properly
 
 Cat.prototype.haveBaby = function (x) {
 	var newCat = new Cat("Baby " + this.name);
